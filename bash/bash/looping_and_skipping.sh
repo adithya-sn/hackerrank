e9bf6b4404db9b1cea2909c0edc0edb2cb5123f0
Adithya S <adi.1994@yahoo.in>
2019-05-21 02:17:44 +0000

    Add new file

---
+++ bash/bash/looping_and_skipping.sh
@@ -0,0 +1,10 @@
+#Your task is to use for loops to display only odd natural numbers from 1 to 99.
+
+#!/bin/bash
+
+for i in {1..99}
+do
+        if [ $((i%2)) != 0 ];then
+                echo $i
+        fi
+done
