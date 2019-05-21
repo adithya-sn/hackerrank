c10c23f314ad5a6d67f96d9d04e383886e90d05b
Adithya S <adi.1994@yahoo.in>
2019-05-21 05:18:18 +0000

    Add new file

---
+++ bash/more_on_conditionals.sh
@@ -0,0 +1,15 @@
+#Given three integers (x, y, and z) representing the three sides of a triangle, identify whether the triangle is Scalene(all unequal), Isosceles(2 sides equal), or Equilateral.
+
+#!/bin/bash
+
+read x; read y; read z
+
+if [ $x = $y ] && [ $y = $z ] && [ $x = $z ]
+then
+    echo "EQUILATERAL"
+elif [ $x -eq $y ] || [ $y -eq $z ] || [ $x -eq $z ]
+then
+    echo "ISOCELES"
+else
+    echo "SCALENE"
+fi
